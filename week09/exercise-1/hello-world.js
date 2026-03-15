51e376a9af5a85741341e05a1821ffe1df5dc2a4
Ibrahim Yondem <ibrahim.y2001@gmail.com>
2026-03-15 09:58:30 -0400

    Lab6 Exercise 1 done

--- week09/exercise-1/hello-world.js
+++ week09/exercise-1/hello-world.js
@@ -1,4 +1,4 @@
-var greeter = function (name) {
-    console.log('Hello, ', +name);
+var greeter = function (firstName, lastName) {
+    console.log("Hello, ".concat(firstName, " ").concat(lastName, "!"));
 };
-greeter('John Smith ');
+greeter('John', 'Smith');
--- week09/exercise-1/hello-world.ts
+++ week09/exercise-1/hello-world.ts
@@ -1,7 +1,6 @@
 
-var greeter = function (name) {
-    console.log('Hello, ', + name);
+let greeter = (firstName, lastName) => {
+    console.log(`Hello, ${firstName} ${lastName}!`);
 }
 
-greeter('John Smith ');
-
+greeter('John', 'Smith');
